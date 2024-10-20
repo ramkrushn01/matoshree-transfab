c6c6938776a7e767324ca0a1aa306bd33bb4d4d9
unknown <ramkrushnsalunkhe99@gmail.com>
2024-10-20 17:54:20 +0530

    complete the home page and navigation bar also for desktop size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
         </div>
       <div className="contact-info">
         <h3>Contact Us:</h3>
-        <h3 className='color-voilet'>Mobile: <span className="color-red">+91 9335949494 </span></h3>
+        <h3 className='color-voilet'>Mobile: <span className="color-red">+91 8335949494 </span></h3>
         <h3 className='sec-number color-red'> +91 8600115570</h3> 
         <h3 className="color-green">Email: <span className="color-blue">matoshree.transfab@gmail.com</span></h3>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { IoIosArrowDown } from "react-icons/io";
 
 export default function Navbar() {
   return (
     <div className="nav-main">
       <ul className="nav-leader">
         <li className="nav-item">Home</li>
-        <li className="nav-item">About Us <FontAwesomeIcon icon="fa-light fa-angle-down" /> </li>
-        <li className="nav-item">Products <FontAwesomeIcon icon="fa-light fa-angle-down" /> </li>
-        <li className="nav-item">Services</li>
+        <li className="nav-item">About Us <IoIosArrowDown /> 
+        <ul className="nav-sub-leader">
+          <li className="nav-sub-leader-item">Company Profile</li>
+          <li className="nav-sub-leader-item">Vision & Mission</li>
+          <li className="nav-sub-leader-item">Quality Policy</li>
+          <li className="nav-sub-leader-item">Our Team</li>
+          <li className="nav-sub-leader-item">Infrastructure & Facilities</li>
+        </ul>
+        </li>
+        <li className="nav-item">Products </li>
+        <li className="nav-item">Services <IoIosArrowDown />
+        <ul className="nav-sub-leader">
+          <li className="nav-sub-leader-item">Repairing</li>
+          <li className="nav-sub-leader-item">Refurbishment</li>
+        </ul>
+        </li>
         <li className="nav-item">Clients</li>
-        <li className="nav-item">Gallery</li>
-        <li className="nav-item">Contact Us & Enquiry</li>
+        <li className="nav-item">Gallery <IoIosArrowDown /> 
+        <ul className="nav-sub-leader">
+          <li className="nav-sub-leader-item">Photo's of Product Internal & Outer</li>
+          <li className="nav-sub-leader-item">Photo's of Workshop</li>
+          <li className="nav-sub-leader-item">Photo's of Testing Machine</li>
+          <li className="nav-sub-leader-item">Photo's of Team</li>
+          <li className="nav-sub-leader-item">Photo's of Office</li>
+        </ul>
+        </li>
+        <li className="nav-item">Contact Us & Enquiry 
+          {/* <IoIosArrowDown />  */}
+        </li>
         <li className="nav-item">Why Us?</li>
       </ul>
     </div>
