d047b52fedd261b454b5fd38f4ea23a85ec36edc
unknown <ramkrushnsalunkhe99@gmail.com>
2024-10-28 21:57:09 +0530

    seperate the sidebar from home.jsx and create new comman file for it

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../css/Home.css";
 import HomeImages from "./common/HomeImages";
+import Sidebar from "./Sidebar";
 
 export default function Home() {
     return (
@@ -94,64 +95,7 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="info-images">
-                    <div className="side-sec">
-                        <h3 className="side-sec-head">
-                            <span className="heading">Manufacturing</span>
-                        </h3>
-                        <div className="head-info">
-                            Supplied <strong>5000+</strong>&nbsp;
-                            <strong>Transformers</strong> to various industries{" "}
-                            <strong>
-                                <em>all over India</em>
-                            </strong>{" "}
-                            since <strong>30+ Years. </strong>
-                        </div>
-                        <img
-                            src="Images/Trans-side-view.jpeg"
-                            alt=""
-                            className="sec-info-img"
-                        />
-                        <img
-                            src="Images/Trans-coil-bundle.jpeg"
-                            alt=""
-                            className="sec-info-img"
-                        />
-                    </div>
-                    <div className="side-sec">
-                        <h3 className="side-sec-head">
-                            <span className="heading">Repairing & Refurbishment</span>
-                        </h3>
-
-                        <img
-                            src="Images/repair1.jpg"
-                            alt=""
-                            className="sec-info-img"
-                        />
-                        <img
-                            src="Images/repair1.jpg"
-                            alt=""
-                            className="sec-info-img"
-                        />
-                    </div>
-                    <div className="side-sec">
-                        <h3 className="side-sec-head">
-                            <span className="heading">Testing</span>
-                        </h3>
-
-                        {/* <img
-                            src="Images/repair1.jpg"
-                            alt=""
-                            className="sec-info-img"
-                        /> */}
-                        <img
-                            src="Images/testing-facility.jpg"
-                            alt=""
-                            className="sec-info-img"
-                        />
-                    </div>
-                    
-                </div>
+                <Sidebar/>
             </div>
         </div>
     );
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import "../css/Home.css";
+
+
+export default function Sidebar() {
+  return (
+    <div className="info-images">
+                    <div className="side-sec">
+                        <h3 className="side-sec-head">
+                            <span className="heading">Manufacturing</span>
+                        </h3>
+                        <div className="head-info">
+                            Supplied <strong>5000+</strong>&nbsp;
+                            <strong>Transformers</strong> to various industries{" "}
+                            <strong>
+                                <em>all over India</em>
+                            </strong>{" "}
+                            since <strong>30+ Years. </strong>
+                        </div>
+                        <img
+                            src="Images/Trans-side-view.jpeg"
+                            alt=""
+                            className="sec-info-img"
+                        />
+                        <img
+                            src="Images/Trans-coil-bundle.jpeg"
+                            alt=""
+                            className="sec-info-img"
+                        />
+                    </div>
+                    <div className="side-sec">
+                        <h3 className="side-sec-head">
+                            <span className="heading">Repairing & Refurbishment</span>
+                        </h3>
+
+                        <img
+                            src="Images/repair1.jpg"
+                            alt=""
+                            className="sec-info-img"
+                        />
+                        <img
+                            src="Images/repair1.jpg"
+                            alt=""
+                            className="sec-info-img"
+                        />
+                    </div>
+                    <div className="side-sec">
+                        <h3 className="side-sec-head">
+                            <span className="heading">Testing</span>
+                        </h3>
+
+                        {/* <img
+                            src="Images/repair1.jpg"
+                            alt=""
+                            className="sec-info-img"
+                        /> */}
+                        <img
+                            src="Images/testing-facility.jpg"
+                            alt=""
+                            className="sec-info-img"
+                        />
+                    </div>
+                    
+                </div> 
+  )
+}
